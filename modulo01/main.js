09fc18f4668c1da50eed89ca3ce10f609a9a437d
Jean Lucas <j3anlucas010@gmail.com>
2025-04-22 15:27:30 -0300

    importando tudo com import * as

--- modulo01/main.js
+++ modulo01/main.js
@@ -1,4 +1,4 @@
-import {sum,multiply} from "./calc.js"
+import * as m2 from "./calc.js" // importa tudo que estivar no calc.js
 
-console.log('4 + 6 =',sum(4,6))
-console.log('4 x 6 =',multiply(4,6))
+console.log('4 + 6 =',m2.sum(6,6))
+console.log('4 x 6 =',m2.multiply(4,6))
---
+++ modulo02/main.js
@@ -0,0 +1,4 @@
+import {sum,multiply} from "./calc.js"
+
+console.log('4 + 6 =',sum(4,6))
+console.log('4 x 6 =',multiply(4,6))
