41fb83033d8b3979d087aff8d9ec3d28b2850f70
Jean Lucas <j3anlucas010@gmail.com>
2025-04-27 14:07:46 -0300

    Adiciona implementação de setInterval( )  para contagem regressiva e mensagem de ano novo

--- modulo04/main.js
+++ modulo04/main.js
@@ -1,4 +1,18 @@
 //executa uma função após um intervalo de tempo especificado
+
 setTimeout(()=>{
         console.log('oi')
 },3000) // tempo em milissegundos
+
+// setInterval() executa uma função após um intervalo de tempo especificado
+
+let valor1 = 10;
+
+const intervalo = setInterval(() => {
+  console.log(valor1);
+  valor1--;
+  if (valor1 == 0) {
+        console.log('Feliz ano novo!!!')
+        clearInterval(intervalo);// Interrompe o intervalo de execuções.
+}
+}, 1000);
